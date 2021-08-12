2f0253fa9c3d8a6a65e92a572398e9d4025a5fbc
Sam'an <samangriffiths47@gmail.com>
2021-08-11 23:32:53 -0500

    final

--- script.js
+++ script.js
@@ -1,199 +1,234 @@
-const canvas = document.getElementById('canvas')
-const context = canvas.getContext('2d')
-const ballz = []
-const ballFallz = []
 const paddleHeight = 10
 const paddleWidth = 75
 const firstHealth = document.getElementById('firstHealth').style
 const secondHealth = document.getElementById('secondHealth').style
 const thirdHealth = document.getElementById('thirdHealth').style
-let paddleX = (canvas.width - paddleWidth * 2) / 2
-let rightPressed = false
-let leftPressed = false
-let gameActive = false
-let playerScore = 0
+const gameOverScreen = document.getElementById('gameOver').style
+const niceJobScreen = document.getElementById('niceJob').style
+const StartScreen = document.getElementById('start').style
+const ballz = []
+let gameActive = true
 
-if (gameActive === false) {
-  // DEFAULT GAME DISPLAY FUNCTIONS///////////////////////////////////////////////////////
-  function randomX(mag) {
-    let x = Math.random()
-    if (x < 0.5) {
-      x *= mag - Math.random() * mag
-    } else {
-      x *= -mag + Math.random() * mag
-    }
-    return x
-  }
-  function randomY(x, mag) {
-    let y = Math.sqrt(mag ** 2 - x ** 2)
-    return -y
-  }
+// if (gameActive === false) {
+//   const canvas = document.getElementById('canvasTwo')
+//   const context = canvas.getContext('2d')
+//   let paddleX = (canvas.width - paddleWidth * 2) / 2
+//   // Game Start Functions////////////////////////////////////////////////////////////////
+//   // Event Listeners
+//   const startButton = document.getElementById('startButton')
+//   const restartButton = document.getElementById('restart')
+//   const restartButton2 = document.getElementById('restartTwo')
+//   startButton.addEventListener('click', function () {
+//     ballz.length = 0
+//     canvas.style.display = 'none'
+//     document.getElementById('canvas').style.display = 'block'
+//     StartScreen.display = 'none'
+//     return (gameActive = true)
+//   })
+//   restartButton.addEventListener('click', function () {
+//     ballz.length = 0
+//     canvas.style.display = 'none'
+//     document.getElementById('canvas').style.display = 'block'
+//     niceJobScreen.display = 'none'
+//     gameActive = true
+//   })
+//   restartButton2.addEventListener('click', function () {
+//     ballz.length = 0
+//     canvas.style.display = 'none'
+//     document.getElementById('canvas').style.display = 'block'
+//     gameOverScreen.display = 'none'
+//     gameActive = true
+//   })
 
-  //draws the paddle onto the canvas
-  drawPaddle = () => {
-    context.beginPath()
-    context.rect(
-      paddleX,
-      canvas.height - paddleHeight,
-      paddleWidth * 2,
-      paddleHeight
-    )
-    context.strokeStyle = 'black'
-    context.stroke()
-    context.fillStyle = 'blue'
-    context.fill()
-    context.closePath()
-  }
+//   // DEFAULT GAME DISPLAY FUNCTIONS///////////////////////////////////////////////////////
+//   function randomX(mag) {
+//     let x = Math.random()
+//     if (x < 0.5) {
+//       x *= mag - Math.random() * mag
+//     } else {
+//       x *= -mag + Math.random() * mag
+//     }
+//     return x
+//   }
+//   function randomY(x, mag) {
+//     let y = Math.sqrt(mag ** 2 - x ** 2)
+//     return -y
+//   }
 
-  wallCollision = (i) => {
-    if (i !== undefined) {
-      if (
-        ballz[i].position.x + ballz[i].r > canvas.width ||
-        ballz[i].position.x - ballz[i].r < 0
-      ) {
-        ballz[i].velocity.x = -ballz[i].velocity.x
-      }
-      if (
-        ballz[i].position.y + ballz[i].r > canvas.height ||
-        ballz[i].position.y - ballz[i].r < 0
-      ) {
-        ballz[i].velocity.y = -ballz[i].velocity.y
-      }
-    }
-  }
+//   //draws the paddle onto the canvas
+//   drawPaddle = () => {
+//     context.beginPath()
+//     context.rect(
+//       paddleX,
+//       canvas.height - paddleHeight,
+//       paddleWidth * 2,
+//       paddleHeight
+//     )
+//     context.strokeStyle = 'black'
+//     context.stroke()
+//     context.fillStyle = 'blue'
+//     context.fill()
+//     context.closePath()
+//   }
 
-  //collision detection between two balls
-  ballOnBallCollision = (b1, b2) => {
-    if (b1.r + b2.r >= b2.position.subtract(b1.position).magnitude()) {
-      return true
-    } else {
-      return false
-    }
-  }
-  //repositions the balls based on the penetration depth and the collision normal
-  noPenetration = (b1, b2) => {
-    // calculates the distance between position vectors
-    let dist = b1.position.subtract(b2.position)
-    //defines the depth of penetration
-    let pen_depth = b1.r + b2.r - dist.magnitude()
-    //undoes penetration
-    let pen_res = dist.unit().multiply(pen_depth / 2)
-    b1.position = b1.position.add(pen_res)
-    b2.position = b2.position.add(pen_res.multiply(-1))
-  }
-  //calculates the balls new velocity vectors after the collision
-  ricochetEffect = (b1, b2) => {
-    //collision normal vector
-    let normal = b1.position.subtract(b2.position).unit()
-    //setting velocity to the angle of the normal vector at original magnitude
-    //(effectively breaking conservation of momentum)
-    b1.velocity = normal.multiply(b1.mag)
-    b2.velocity = normal.multiply(b2.mag).multiply(-1)
-  }
+//   wallCollision = (i) => {
+//     if (i !== undefined) {
+//       if (
+//         ballz[i].position.x + ballz[i].r > canvas.width ||
+//         ballz[i].position.x - ballz[i].r < 0
+//       ) {
+//         ballz[i].velocity.x = -ballz[i].velocity.x
+//       }
+//       if (
+//         ballz[i].position.y + ballz[i].r > canvas.height ||
+//         ballz[i].position.y - ballz[i].r < 0
+//       ) {
+//         ballz[i].velocity.y = -ballz[i].velocity.y
+//       }
+//     }
+//   }
 
-  class Vector {
-    constructor(x, y) {
-      this.x = x
-      this.y = y
-    }
-    //allows two vectors to be added together
-    add(v) {
-      return new Vector(this.x + v.x, this.y + v.y)
-    }
-    //allows a vector to be subtracted from the vector
-    subtract(v) {
-      return new Vector(this.x - v.x, this.y - v.y)
-    }
-    //calculates the magnitude of the vector
-    magnitude() {
-      return Math.sqrt(this.x ** 2 + this.y ** 2)
-    }
-    // allows the vector to be multiplied by a number
-    multiply(n) {
-      return new Vector(this.x * n, this.y * n)
-    }
-    // finds the normal vector of a collision
-    normal() {
-      return new Vector(-this.y, this.x).unit()
-    }
-    // sets magnitude to zero, so magnitude goes unaffected
-    // when multiplying a vector by the normal
-    unit() {
-      if (this.magnitude() === 0) {
-        return new Vector(0, 0)
-      } else {
-        return new Vector(this.x / this.magnitude(), this.y / this.magnitude())
-      }
-    }
-    // finds the dot product of two vectors
-    static dot(v1, v2) {
-      return v1.x * v2.x + v1.y * v2.y
-    }
-  }
+//   //collision detection between two balls
+//   ballOnBallCollision = (b1, b2) => {
+//     if (b1.r + b2.r >= b2.position.subtract(b1.position).magnitude()) {
+//       return true
+//     } else {
+//       return false
+//     }
+//   }
+//   //repositions the balls based on the penetration depth and the collision normal
+//   noPenetration = (b1, b2) => {
+//     // calculates the distance between position vectors
+//     let dist = b1.position.subtract(b2.position)
+//     //defines the depth of penetration
+//     let pen_depth = b1.r + b2.r - dist.magnitude()
+//     //undoes penetration
+//     let pen_res = dist.unit().multiply(pen_depth / 2)
+//     b1.position = b1.position.add(pen_res)
+//     b2.position = b2.position.add(pen_res.multiply(-1))
+//   }
+//   //calculates the balls new velocity vectors after the collision
+//   ricochetEffect = (b1, b2) => {
+//     //collision normal vector
+//     let normal = b1.position.subtract(b2.position).unit()
+//     //setting velocity to the angle of the normal vector at original magnitude
+//     //(effectively breaking conservation of momentum)
+//     b1.velocity = normal.multiply(b1.mag)
+//     b2.velocity = normal.multiply(b2.mag).multiply(-1)
+//   }
 
-  //class for slowest ball
-  class slowBall {
-    constructor() {
-      this.r = 30
-      this.position = new Vector(canvas.width / 2, canvas.height - 100)
-      this.mag = 3
-      this.vx = randomX(this.mag)
-      this.vy = randomY(this.vx, this.mag)
-      this.velocity = new Vector(this.vx, this.vy)
+//   class Vector {
+//     constructor(x, y) {
+//       this.x = x
+//       this.y = y
+//     }
+//     //allows two vectors to be added together
+//     add(v) {
+//       return new Vector(this.x + v.x, this.y + v.y)
+//     }
+//     //allows a vector to be subtracted from the vector
+//     subtract(v) {
+//       return new Vector(this.x - v.x, this.y - v.y)
+//     }
+//     //calculates the magnitude of the vector
+//     magnitude() {
+//       return Math.sqrt(this.x ** 2 + this.y ** 2)
+//     }
+//     // allows the vector to be multiplied by a number
+//     multiply(n) {
+//       return new Vector(this.x * n, this.y * n)
+//     }
+//     // finds the normal vector of a collision
+//     normal() {
+//       return new Vector(-this.y, this.x).unit()
+//     }
+//     // sets magnitude to zero, so magnitude goes unaffected
+//     // when multiplying a vector by the normal
+//     unit() {
+//       if (this.magnitude() === 0) {
+//         return new Vector(0, 0)
+//       } else {
+//         return new Vector(this.x / this.magnitude(), this.y / this.magnitude())
+//       }
+//     }
+//     // finds the dot product of two vectors
+//     static dot(v1, v2) {
+//       return v1.x * v2.x + v1.y * v2.y
+//     }
+//   }
 
-      this.collisionCounter = []
-      ballz.push(this)
-    }
-    //draws ball onto the canvas
-    drawBall() {
-      context.beginPath()
-      context.arc(this.position.x, this.position.y, this.r, 0, Math.PI * 2)
-      context.strokeStyle = `black`
-      context.stroke()
-      context.fillStyle = `blue`
-      context.fill()
-    }
-    //handles movement
-    move() {
-      this.position.x += this.velocity.x
-      this.position.y += this.velocity.y
-    }
-  }
+//   //class for slowest ball
+//   class slowBall {
+//     constructor() {
+//       this.r = 30
+//       this.position = new Vector(canvas.width / 2, canvas.height - 100)
+//       this.mag = 3
+//       this.vx = randomX(this.mag)
+//       this.vy = randomY(this.vx, this.mag)
+//       this.velocity = new Vector(this.vx, this.vy)
 
-  let counter = 0
-  ball = new slowBall()
-  let throwball = setInterval(function () {
-    ball = new slowBall()
-    ++counter
-    if (counter > 2) {
-      clearInterval(throwball)
-    }
-  }, 5000)
+//       this.collisionCounter = []
+//       ballz.push(this)
+//     }
+//     //draws ball onto the canvas
+//     drawBall() {
+//       context.beginPath()
+//       context.arc(this.position.x, this.position.y, this.r, 0, Math.PI * 2)
+//       context.strokeStyle = `black`
+//       context.stroke()
+//       context.fillStyle = `blue`
+//       context.fill()
+//     }
+//     //handles movement
+//     move() {
+//       this.position.x += this.velocity.x
+//       this.position.y += this.velocity.y
+//     }
+//   }
 
-  function draw() {
-    context.clearRect(0, 0, canvas.width, canvas.height)
-    ballz.forEach((b, index) => {
-      b.drawBall()
-      b.move()
-      wallCollision(index)
-      for (let i = index + 1; i < ballz.length; i++) {
-        if (ballOnBallCollision(ballz[index], ballz[i])) {
-          noPenetration(ballz[index], ballz[i])
-          ricochetEffect(ballz[index], ballz[i])
-        }
-      }
-    })
-    drawPaddle()
-    requestAnimationFrame(draw)
-  }
-  requestAnimationFrame(draw)
-}
+//   let counter = 0
+//   ball = new slowBall()
+//   let throwball = setInterval(function () {
+//     ball = new slowBall()
+//     ++counter
+//     if (counter > 2) {
+//       clearInterval(throwball)
+//     }
+//   }, 5000)
+
+//   function draw() {
+//     context.clearRect(0, 0, canvas.width, canvas.height)
+//     ballz.forEach((b, index) => {
+//       b.drawBall()
+//       b.move()
+//       wallCollision(index)
+//       for (let i = index + 1; i < ballz.length; i++) {
+//         if (ballOnBallCollision(ballz[index], ballz[i])) {
+//           noPenetration(ballz[index], ballz[i])
+//           ricochetEffect(ballz[index], ballz[i])
+//         }
+//       }
+//     })
+//     drawPaddle()
+//     requestAnimationFrame(draw)
+//   }
+//   requestAnimationFrame(draw)
+// }
 
 if (gameActive === true) {
+  const canvas = document.getElementById('canvas')
+  const context = canvas.getContext('2d')
+  const ballFallz = []
+  let paddleX = (canvas.width - paddleWidth * 2) / 2
+  let rightPressed = false
+  let leftPressed = false
+  let playerScore = 0
+  const pastScores = []
   //Global Functions//////////////////////////////////////////////////////////////////////////////////
   // Interactivity
 
   threeStrikes = () => {
+    console.log(ballFallz.length)
     if (ballFallz.length === 1) {
       thirdHealth.display = 'none'
       secondHealth.backgroundColor = 'yellow'
@@ -267,12 +302,12 @@ if (gameActive === true) {
           if (ballz[i].mag === 3) {
             playerScore += 10
           }
-          if (ballz[i].mag === 3) {
-            playerScore += 10
-          }
-          if (ballz[i].mag === 3) {
-            playerScore += 10
-          }
+          // if (ballz[i].mag === 3) {
+          //   playerScore += 10
+          // }
+          // if (ballz[i].mag === 3) {
+          //   playerScore += 10
+          // }
         } else {
           ballFallz.push('1')
           ballz.length = 0
@@ -327,7 +362,7 @@ if (gameActive === true) {
   }
   // function for mouse movement
   mouseMoveHandler = (e) => {
-    let relativeX = e.clientX - canvas.offsetLeft
+    let relativeX = e.clientX - canvas.width / 2 + 40
     if (relativeX > 0 && relativeX < canvas.width) {
       paddleX = relativeX - paddleWidth
     }
@@ -413,7 +448,7 @@ if (gameActive === true) {
       context.arc(this.position.x, this.position.y, this.r, 0, Math.PI * 2)
       context.strokeStyle = `black`
       context.stroke()
-      context.fillStyle = `grey`
+      context.fillStyle = `blue`
       context.fill()
     }
     //handles movement
